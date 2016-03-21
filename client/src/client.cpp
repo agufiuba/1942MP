f35c2c16b9e7ab093eca767f8ff3c594c38be502
makushi <maximiliano.scoccimarro@gmail.com>
2016-03-21 11:38:48 -0300

    Modify client to use automated menu.

--- client/src/client.cpp
+++ client/src/client.cpp
@@ -1,53 +1,25 @@
-#include <iostream>
+#ifndef MENU_H
+#include "../libs/menu/Menu.h"
+#endif
 
-using namespace std;
-
-typedef unsigned short ushort;
-
-void displayMenu();
-ushort inputOption();
-
-void displayMenu() {
-  cout << "----\tMenu de opciones de Cliente\t----" << endl;
-  cout << "  1. Conectar" << endl;
-  cout << "  2. Desconectar" << endl;
-  cout << "  3. Ciclar" << endl;
-  cout << "  4. Salir" << endl << endl;
-  cout << " Seleccione una opcion para continuar: ";
-}
-
-ushort inputOption(ushort &option) {
-  cin >> option;
-}
+void connect(){}
+void disconnect(){}
+void cycle(){}
+void exitPgm(){}
 
 int main(int argc, char* argv[]) {
   
   const char* fileName = argv[1]; 
-  
   if(!fileName) fileName = "default-client.xml";
 
-  bool quit = false;
-  ushort option = 0;
+  Menu clientMenu("Menu de opciones del Cliente"); 
 
-  while(!quit) {
-    displayMenu();
-    inputOption(option);
+  clientMenu.addOption("Conectar", connect);
+  clientMenu.addOption("Desconectar", disconnect);
+  clientMenu.addOption("Ciclar", cycle);
+  clientMenu.addOption("Salir", exitPgm);
 
-    switch(option) {
-      case 1: 
-	break;
-      case 2:
-	break;
-      case 3:
-	break;
-      case 4:
-	quit = true;
-	cout << endl << "Cerrando cliente..." << endl;
-	break;
-      default:
-	cout << "La opcion ingresada es invalida!" << endl;
-    }
-  }
+  clientMenu.display();
 
   return 0;
 }
