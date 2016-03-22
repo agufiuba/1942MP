baedf9aa047338bcf7407e8d1ccf0b167191fbb9
agufiuba <agufiuba@gmail.com>
2016-03-22 14:17:02 -0300

    Implementa módulo para valores constantes

---
+++ src/utils/K.h
@@ -0,0 +1,12 @@
+#ifndef SRC_UTILS_K_H_
+#define SRC_UTILS_K_H_
+
+class K {
+public:
+	static const unsigned short int typeInt = 1;
+	const static unsigned short int typeString = 2;
+	const static unsigned short int typeDouble = 3;
+	const static unsigned short int typeChar = 4;
+};
+
+#endif
