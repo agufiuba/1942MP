f7a35c88fa1ea4bd374fd5b049376d586f137636
makushi <maximiliano.scoccimarro@gmail.com>
2016-03-25 20:04:00 -0300

    Integrate client with menu. Create connect option function. Create disconnect option function.

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -7,23 +7,36 @@
 #include <cstdlib> /* exit() */
 #include <cstring> /* memset(), bzero() */
 #include <iostream>
+#include "../libs/menu/Menu.h"
 
 using namespace std;
 
-int main() {
-  
+int gfd = 0;
+bool connected = false;
+
+Menu clientMenu("Menu de opciones del Cliente");
+
+void srvConnect() {
+  if(connected) {
+    cout << endl << "Ya hay una conexion activa" << endl;
+    clientMenu.display();
+    return;
+  }
+
   const int PORT = 5340;
   const int MAX_DATA_SIZE = 100; /* Max. number of bytes for recv */
   int sfd, numBytesRead;
   char buf[MAX_DATA_SIZE]; /* Received text buffer  */
   struct sockaddr_in server; /* Server address info */
-  const char* IP = "192.168.1.101";
+  const char* IP = "192.168.1.108";
 
   /* Create socket */
   if((sfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
     cout << "socket error" << endl;
     exit(-1);
   }
+
+  gfd = sfd;
 
   server.sin_family = AF_INET;
   server.sin_port = htons(PORT);
@@ -35,18 +48,11 @@ int main() {
   bzero(&(server.sin_zero), 8);
 
   /* Connect to server */
-  bool connected = false;
   short triesLeft = 3;
 
-  while(!connected) {
-    if(triesLeft == 0) {
-      cout << "No se pudo establecer una conexion con el servidor: " << IP 
-           << endl << "Por favor intente nuevamente mas tarde." << endl;
-      exit(-1);
-    }
-
+  while(!connected && triesLeft > 0) {
     if(connect(sfd, (struct sockaddr*) &server, sizeof(struct sockaddr)) == -1) {
-      cout << "Error de conexion." << endl << "Reintentando conexion..." << endl << endl;
+      cout << endl << "Error de conexion." << endl << "Reintentando conexion..." << endl;
       triesLeft--;
       /* 5s delay for retry */
       usleep(5000000);
@@ -54,16 +60,55 @@ int main() {
       connected = true;
       cout << "Se establecio una conexion con: " << IP << endl;
     }
-  }
 
-  if((numBytesRead = recv(sfd, buf, MAX_DATA_SIZE, 0)) == -1) {
-    cout << "recv error" << endl;
-    exit(-1);
+    if(triesLeft == 0) {
+      cout << endl << "No se pudo establecer una conexion con el servidor: " << IP 
+           << endl << "Por favor intente nuevamente mas tarde." << endl;
+    }
   }
+  
+  clientMenu.display();
 
-  buf[numBytesRead] = '\0';
-  cout << "Mensaje del servidor: " << buf << endl;
-  close(sfd);
+  /*
+  while(connected) {
+    if((numBytesRead = recv(sfd, buf, MAX_DATA_SIZE, 0)) == -1) {
+      cout << "recv error" << endl;
+      exit(-1);
+    }
+
+    if(numBytesRead) {
+      buf[numBytesRead] = '\0';
+      cout << "Mensaje del servidor: " << buf << endl;
+    }
+  }*/
+  //close(sfd);
+}
+
+void srvDisconnect() {
+  if(connected) {
+    close(gfd);  
+    connected = false;
+    cout << endl << "Se cerro la conexion con el servidor." << endl;
+  } else {
+    cout << endl << "No hay una conexion activa" << endl;
+  }
+  clientMenu.display();
+}
+
+void cycle() {}
+
+void exitPgm() {}
+
+int main(int argc, char* argv[]) {
+  
+  const char* fileName = argv[1] ? argv[1] : "default.xml";
+
+  clientMenu.addOption("Conectar", srvConnect);
+  clientMenu.addOption("Desconectar", srvDisconnect);
+  clientMenu.addOption("Ciclar", cycle);
+  clientMenu.addOption("Salir", exitPgm);
+
+  clientMenu.display();
 
   return 0;
 }
