a1cc6d16453b2b4c46b0c35ebc65d4c5fa3a057c
makushi <maximiliano.scoccimarro@gmail.com>
2016-04-01 12:18:45 -0300

    Fix client connection retries. Fix client and server indentation.

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -21,149 +21,149 @@ const int MSG_QUANTITY = 4;
 string msgQueue[MSG_QUANTITY] = { "hola", "mundo", "chau", "gente" };
 
 void closeConnection() {
-	close(gfd);
-	connected = false;
-	cout << endl << warning("Se cerro la conexion con el servidor.") << endl;
+  close(gfd);
+  connected = false;
+  cout << endl << warning("Se cerro la conexion con el servidor.") << endl;
 }
 
 void srvConnect() {
-	if (connected) {
-		cout << endl << warning("Ya hay una conexion activa") << endl;
-		return;
-	}
+  if (connected) {
+    cout << endl << warning("Ya hay una conexion activa") << endl;
+    return;
+  }
 
-	const int PORT = 5340;
-	const int MAX_DATA_SIZE = 100; /* Max. number of bytes for recv */
-	int sfd, numBytesRead;
-	char buf[MAX_DATA_SIZE]; /* Received text buffer  */
-	struct sockaddr_in server; /* Server address info */
-	const char* IP = "127.0.0.1";
+  const int PORT = 5340;
+  const int MAX_DATA_SIZE = 100; /* Max. number of bytes for recv */
+  int sfd, numBytesRead;
+  char buf[MAX_DATA_SIZE]; /* Received text buffer  */
+  struct sockaddr_in server; /* Server address info */
+  const char* IP = "127.0.0.1";
 
-	/* Create socket */
-	if ((sfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-		cout << "socket error" << endl;
-		exit(-1);
-	}
+  /* Create socket */
+  if ((sfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+    cout << "socket error" << endl;
+    exit(-1);
+  }
 
-	gfd = sfd;
+  gfd = sfd;
 
-	server.sin_family = AF_INET;
-	server.sin_port = htons(PORT);
-	if ((inet_aton(IP, &server.sin_addr)) == 0) {
-		cout << "invalid IP" << endl;
-		exit(-1);
-	}
+  server.sin_family = AF_INET;
+  server.sin_port = htons(PORT);
+  if ((inet_aton(IP, &server.sin_addr)) == 0) {
+    cout << "invalid IP" << endl;
+    exit(-1);
+  }
 
-	bzero(&(server.sin_zero), 8);
+  bzero(&(server.sin_zero), 8);
 
-	/* Connect to server */
-	short triesLeft = 3;
+  /* Connect to server */
+  short triesLeft = 3;
 
-	while (!connected && triesLeft > 0) {
-		if (connect(sfd, (struct sockaddr*) &server, sizeof(struct sockaddr))
-		    == -1) {
-			cout << endl << warning("Error de conexion.") << endl
-			    << "Reintentando conexion..." << endl;
-			triesLeft--;
-			/* 5s delay for retry */
-			usleep(5000000);
-		} else {
-			connected = true;
-		}
+  while (!connected && triesLeft) {
+    if (connect(sfd, (struct sockaddr*) &server, sizeof(struct sockaddr))
+	== -1) {
+      triesLeft--;
+      /* 5s delay for retry */
+      if(triesLeft) {
+	cout << endl << warning("Error de conexion.") << endl
+	     << "Reintentando conexion..." << endl;
+	usleep(5000000);
+      } else {
+	cout << endl << warning("No se pudo establecer una conexion con el servidor: ")
+	     << IP << endl << "Por favor intente nuevamente mas tarde." << endl;
+	return;
+      }
+    } else {
+      connected = true;
+    }
+  }
 
-		if (triesLeft == 0) {
-			cout << endl
-			    << warning("No se pudo establecer una conexion con el servidor: ")
-			    << IP << endl << "Por favor intente nuevamente mas tarde." << endl;
-		}
-	}
+  // while(connected) {
+  if ((numBytesRead = recv(sfd, buf, MAX_DATA_SIZE, 0)) == -1) {
+    cout << "recv error" << endl;
+    exit(-1);
+  }
 
-	// while(connected) {
-	if ((numBytesRead = recv(sfd, buf, MAX_DATA_SIZE, 0)) == -1) {
-		cout << "recv error" << endl;
-		exit(-1);
-	}
-
-	if (numBytesRead) {
-		cout << endl << notice("Se establecio una conexion con: ") << IP << endl;
-		buf[numBytesRead] = '\0';
-		cout << "Mensaje del servidor: " << buf << endl;
-	} else {
-		cout << endl << warning("Se perdio la conexion con el servidor.") << endl;
-		connected = false;
-		close(sfd);
-	}
-	//}
+  if (numBytesRead) {
+    cout << endl << notice("Se establecio una conexion con: ") << IP << endl;
+    buf[numBytesRead] = '\0';
+    cout << "Mensaje del servidor: " << buf << endl;
+  } else {
+    cout << endl << warning("Se perdio la conexion con el servidor.") << endl;
+    connected = false;
+    close(sfd);
+  }
+  //}
 }
 
 void sendData(string data, int dataLength) {
-	if (send(gfd, data.c_str(), dataLength, 0) == -1) {
-		cout << "send error" << endl;
-	}
+  if (send(gfd, data.c_str(), dataLength, 0) == -1) {
+    cout << "send error" << endl;
+  }
 }
 
 void srvDisconnect() {
-	if (connected) {
-		closeConnection();
-	} else {
-		cout << endl << warning("No hay una conexion activa") << endl;
-	}
+  if (connected) {
+    closeConnection();
+  } else {
+    cout << endl << warning("No hay una conexion activa") << endl;
+  }
 }
 
 void exitPgm() {
-	if (connected)
-		closeConnection();
+  if (connected)
+    closeConnection();
 
-	cout << endl << warning("Cerrando el cliente...") << endl;
-	exit(1);
+  cout << endl << warning("Cerrando el cliente...") << endl;
+  exit(1);
 }
 
 void sendMsg(int id) {
-	if (!connected) {
-		cout << endl
-		    << warning("Para mandar un mensaje debe estar conectado al servidor.")
-		    << endl;
-		return;
-	}
+  if (!connected) {
+    cout << endl
+      << warning("Para mandar un mensaje debe estar conectado al servidor.")
+      << endl;
+    return;
+  }
 
-	string data = msgQueue[id];
-	int dataLength = msgQueue[id].length();
-	cout << endl << "Se envio '" << notice(data) << "' al servidor" << endl;
-	sendData(data, dataLength);
+  string data = msgQueue[id];
+  int dataLength = msgQueue[id].length();
+  cout << endl << "Se envio '" << notice(data) << "' al servidor" << endl;
+  sendData(data, dataLength);
 }
 
 void addMsgOptions() {
-	for (int i = 0; i < MSG_QUANTITY; i++) {
-		string optionName = "Enviar mensaje " + to_string(i) + " ";
-		clientMenu.addOption(optionName, sendMsg, i);
-	}
+  for (int i = 0; i < MSG_QUANTITY; i++) {
+    string optionName = "Enviar mensaje " + to_string(i) + " ";
+    clientMenu.addOption(optionName, sendMsg, i);
+  }
 }
 
 void cycle() {
-	int timeout = 0;
-	cout << "Ingrese duracion (en milisegundos): ";
-	cin >> timeout;
+  int timeout = 0;
+  cout << "Ingrese duracion (en milisegundos): ";
+  cin >> timeout;
 
-	// TODO: implement timer
-	for (int i = 0; i < MSG_QUANTITY; i++) {
-		sendMsg(i);
-		if (i != MSG_QUANTITY - 1) {
-			usleep(timeout * 1000);
-		}
-	}
+  // TODO: implement timer
+  for (int i = 0; i < MSG_QUANTITY; i++) {
+    sendMsg(i);
+    if (i != MSG_QUANTITY - 1) {
+      usleep(timeout * 1000);
+    }
+  }
 }
 
 int main(int argc, char* argv[]) {
 
-	const char* fileName = argv[1] ? argv[1] : "default.xml";
+  const char* fileName = argv[1] ? argv[1] : "default.xml";
 
-	clientMenu.addOption("Conectar", srvConnect);
-	clientMenu.addOption("Desconectar", srvDisconnect);
-	addMsgOptions();
-	clientMenu.addOption("Ciclar", cycle);
-	clientMenu.addOption("Salir", exitPgm);
+  clientMenu.addOption("Conectar", srvConnect);
+  clientMenu.addOption("Desconectar", srvDisconnect);
+  addMsgOptions();
+  clientMenu.addOption("Ciclar", cycle);
+  clientMenu.addOption("Salir", exitPgm);
 
-	clientMenu.display();
+  clientMenu.display();
 
-	return 0;
+  return 0;
 }
