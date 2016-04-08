d3846842f4ff83fd0cc5cd7b1b3c79dd9e73a994
keynaka <keynaka@gmail.com>
2016-04-08 01:00:03 -0300

    Dejo en el Default.cpp la IP 127.0.0.1 para que se pueda probar en la misma maquina

--- src/utils/Defaults.cpp
+++ src/utils/Defaults.cpp
@@ -6,5 +6,5 @@ const int Defaults::serverMaxClients = 5;
 const int Defaults::serverPort = 8080;
 //const int Defaults::serverPort = 5340;
 const unsigned short int Defaults::defaultType = K::typeString;
-//const char* Defaults::serverIP = "127.0.0.1";
-const char* Defaults::serverIP = "192.168.1.1";
+const char* Defaults::serverIP = "127.0.0.1";
+//const char* Defaults::serverIP = "192.168.1.1";
