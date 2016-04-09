6ea87d3adc403fa01ca4bd7cdd5e39da16e5d850
makushi <maximiliano.scoccimarro@gmail.com>
2016-04-09 14:01:13 -0300

    Change Menu function argument to string.

--- src/libs/menu/Menu.h
+++ src/libs/menu/Menu.h
@@ -9,7 +9,7 @@ class Menu {
     std::string title;
     std::map<int, std::string> options;
     std::map<int, std::function<void()>> actions;
-    std::map<int, std::map<int, std::function<void(int)>> > iactions;
+    std::map<int, std::map<std::string, std::function<void(std::string)>> > iactions;
     int optionNumber;
     bool validInput(std::string input);
     int requireOption();
@@ -18,7 +18,7 @@ class Menu {
   public:
     Menu(std::string title);
     void addOption(std::string optionName, std::function<void()>);
-    void addOption(std::string optionName, std::function<void(int)>, int);
+    void addOption(std::string optionName, std::function<void(std::string)>, std::string);
     void removeOption(std::string optionName);
     void display();
 };
