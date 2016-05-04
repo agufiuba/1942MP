2427e9fde4b6ede5648f4e039bf3b9540b94c798
makushi <maximiliano.scoccimarro@gmail.com>
2016-05-03 21:08:59 -0300

    Use renders before while to avoid multiple loading. Remove unused timer.

--- src/initialWindow/initialWindow.cpp
+++ src/initialWindow/initialWindow.cpp
@@ -7,62 +7,50 @@ using namespace std;
 
 int main(int argc, char **argv) {
 
-	const int WINDOW_WIDTH = 600; //TODO: aca hay que cambiar a lo del escenario
-	const int WINDOW_HEIGHT = 600;
-	const char* WINDOW_TITLE = "1942MP Arcade";
+  const int WINDOW_WIDTH = 600; //TODO: aca hay que cambiar a lo del escenario
+  const int WINDOW_HEIGHT = 600;
+  const char* WINDOW_TITLE = "1942MP Arcade";
 
-  const int FRAMES_PER_SECOND = 10;
+  bool quit = false;
 
-	bool quit = false;
+  XM_SDL* sdlHandler = new XM_SDL( SDL_INIT_EVERYTHING);
 
-  Timer fps;
+  // Create window
+  if (sdlHandler->createWindow(WINDOW_TITLE, WINDOW_WIDTH, WINDOW_HEIGHT)) {
+    SDL_Event event;
+    SDL_Renderer* renderer = sdlHandler->getRenderer();
 
-	XM_SDL* sdlHandler = new XM_SDL( SDL_INIT_EVERYTHING);
+    Texture * logoPrincipal = new Texture();
+    SDL_Rect* exactImage = new SDL_Rect();
+    exactImage->x = 0;
+    exactImage->y = 0;
+    exactImage->w = 250;
+    exactImage->h = 100;
 
-	// Create window
-	if (sdlHandler->createWindow(WINDOW_TITLE, WINDOW_WIDTH, WINDOW_HEIGHT)) {
-		SDL_Event event;
-		SDL_Renderer* renderer = sdlHandler->getRenderer();
+    if (!logoPrincipal->loadFromFile("windowImages/1942logoPrincipal.bmp", renderer)) {
+      printf("Failed to load logoPrincipal texture image!\n");
+      return false;
+    }
 
-		Texture * logoPrincipal = new Texture();
-		SDL_Rect* exactImage = new SDL_Rect();
-		exactImage->x = 0;
-		exactImage->y = 0;
-		exactImage->w = 250;
-		exactImage->h = 100;
+    // Set window background
+    sdlHandler->setWindowBG(0, 0, 0);
 
-		if (!logoPrincipal->loadFromFile("initialWindow/windowImages/1942logoPrincipal.bmp", renderer)) {
-			printf("Failed to load logoPrincipal texture image!\n");
-			return false;
-		}
+    logoPrincipal->render(175,75,renderer,exactImage);
 
-		while (!quit) {
-      fps.correr();
-			// Get events
-			while (sdlHandler->nextEvent(&event)) {
+    sdlHandler->updateWindow();
 
-				if (event.type == SDL_QUIT)
-					quit = true;
-			}
-
-			// Set window background
-			sdlHandler->setWindowBG(0, 0, 0);
-
-			logoPrincipal->render(175,75,renderer,exactImage);
-
-			sdlHandler->updateWindow();
-
-      if( fps.tiempoActual() < 1000 / FRAMES_PER_SECOND ){
-	SDL_Delay( ( 1000 / FRAMES_PER_SECOND ) - fps.tiempoActual() );
+    while (!quit) {
+      // Get events
+      while (sdlHandler->nextEvent(&event)) {
+	if (event.type == SDL_QUIT)
+	  quit = true;
       }
+    }
+  }
 
-		}
+  delete sdlHandler;
 
-	}
-
-	delete sdlHandler;
-
-return 0;
+  return 0;
 }
 
 
