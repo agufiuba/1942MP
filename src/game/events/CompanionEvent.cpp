bf2d95fd397df899559d36b29489d5d187baa18d
keynaka <keynaka@gmail.com>
2016-05-10 17:27:09 -0300

    Agrego strcpy en evento

--- src/game/events/CompanionEvent.cpp
+++ src/game/events/CompanionEvent.cpp
@@ -3,53 +3,62 @@
 Evento* CompanionEvent::right(string n) {
 		Evento* e = new Evento();
     e->value = DERECHA;
+    strcpy(e->name, n.c_str());
     return e;
 }
 
 Evento* CompanionEvent::left(string n) {
 		Evento* e = new Evento();
     e->value = IZQUIERDA;
+    strcpy(e->name, n.c_str());
     return e;
 }
 
 Evento* CompanionEvent::up(string n) {
 		Evento* e = new Evento();
     e->value = ARRIBA;
+    strcpy(e->name, n.c_str());
     return e;
 }
 
 Evento* CompanionEvent::down(string n) {
 		Evento* e = new Evento();
     e->value = ABAJO;
+    strcpy(e->name, n.c_str());
     return e;
 }
 
 Evento* CompanionEvent::shot(string n) {
 		Evento* e = new Evento();
     e->value = DISPARO;
+    strcpy(e->name, n.c_str());
     return e;
 }
 
 Evento* CompanionEvent::roll(string n) {
 		Evento* e = new Evento();
     e->value = VUELTERETA;
+    strcpy(e->name, n.c_str());
     return e;
 }
 
 Evento* CompanionEvent::ready(string n) {
     Evento* e = new Evento();
     e->value = LISTO;
+    strcpy(e->name, n.c_str());
     return e;
 }
 
 Evento* CompanionEvent::restart(string n) {
 		Evento* e = new Evento();
     e->value = REINICIAR;
+    strcpy(e->name, n.c_str());
     return e;
 }
 
 Evento* CompanionEvent::quit(string n) {
 		Evento* e = new Evento();
     e->value = SALIR;
+    strcpy(e->name, n.c_str());
     return e;
 }
