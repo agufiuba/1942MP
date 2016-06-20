9ed648a739964bbc7bda44ace5b43f817c2b5ea3
makushi <maximiliano.scoccimarro@gmail.com>
2016-06-19 23:25:08 -0300

    Add Medium sized enemy class.

---
+++ src/game/model/ServerAvionEnemigoMedio.h
@@ -0,0 +1,33 @@
+#ifndef SRC_GAME_MODEL_SERVERAVIONENEMIGOMEDIO_H_
+#define SRC_GAME_MODEL_SERVERAVIONENEMIGOMEDIO_H_
+
+#include <iostream>
+#include <ctime>
+#include <chrono>
+#include "../view/AvionView.h"
+#include "Vivible.h"
+#include "../controller/Timer.h"
+#include "Resolucion.h"
+#include "../model/Posicion.h"
+#include "../../libs/data/player_data.h"
+#include "../../xml/conf/AvionConf.h"
+#include <string>
+#include "Misil.h"
+#include "../view/ExplosionView.h"
+#include "../view/Screen.h"
+#include "../controller/ControllerMissilesEnemy.h"
+#include "../model/ServerAvionEnemigo.h"
+
+class ServerAvionEnemigoMedio :public ServerAvionEnemigo{
+
+private:
+  int randomSeed;
+  int dAnt;
+  void refreshSeed();
+public:
+	ServerAvionEnemigoMedio( int id, Posicion* posicionInicial);
+	~ServerAvionEnemigoMedio();
+	EnemyData* vivir();
+};
+
+#endif 
